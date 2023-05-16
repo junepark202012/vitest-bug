f5015b33fbce6363fc7d7cc9c1202f9cdd8be48d
June Park <junepark202012@gmail.com>
2023-05-16 23:50:36 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+    includeSource: ["src/**/*.{js,ts,svelte}"],
+  },
+});
